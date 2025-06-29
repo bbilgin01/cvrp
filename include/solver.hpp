0a65bb4e561bba725e7d39bb32d5fe99b479bf84
Baturalp Bilgin <bab@Baturalps-MacBook-Air.local>
2025-06-29 23:06:00 +0300

    solver header

---
+++ include/solver.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+#include "routing_data.hpp"
+#include "vehicle.hpp"
+
+
+class Solver{
+
+private:
+    RoutingData data;
+    std::vector<Vehicle> vehicles;
+
+public : 
+    Solver(const RoutingData& data_, int vehicle_count, int vehicle_capacity);
+    void solve();
+    void print_solution()const; 
+
+};
